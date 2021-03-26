cdc664f418ec1f99c5c161c5ae750701115a2108
Andrew Gailey <agailey@cray.com>
2021-03-26 10:38:33 -0500

    ASICFWDG-1582 feature: format adjustment for definitions that will be upstreamed with drivers
    automated-update: 2025-05-13T21:32:57+02:00
    Files:
      lib/casuc/cuc_cxi.h
      lib/craypldm/pldm_cxi.h
    (cherry picked from commit 87583f43ba5cd7ac8cbdaa9186e314b57e0ce04e)
    Signed-off-by: James Swaro <james.swaro@hpe.com>

---
+++ lib/casuc/cuc_cxi.h
@@ -0,0 +1,250 @@
+/* SPDX-License-Identifier: MIT */
+/* Copyright 2021 Hewlett Packard Enterprise Development LP */
+
+/* This file defines a set of commands and  request and response packet formats for
+ * communicating with the Cassini uC over USB, SMBus, or the HSN.
+ */
+
+#ifndef CUC_CXI_H
+#define CUC_CXI_H
+
+#define CUC_DATA_BYTES  253
+
+/**
+ * struct cuc_pkt - Cassini uC Packet Format
+ *
+ * This common packet format can be used to communicate with the uC across different
+ * physical interfaces (USB, SMBus, HSN). The SMBus interface is the lowest
+ * common denominator so this is pretty much modeled after the block read/write
+ * functions. We'll leave integrity checks to the physical interfaces.
+ */
+struct cuc_pkt {
+	u8 cmd;                   /* The command to be sent */
+	u8 count;                 /* The number of bytes to follow (including 'type') */
+	u8 type;                  /* Packet type */
+	u8 data[CUC_DATA_BYTES];  /* Data to be sent */
+} __packed;
+
+enum {
+	CUC_CMD_PING = 0,                       /* Perform a simple ping to the uC firmware */
+	CUC_CMD_GET_FRU = 25,                   /* Get the FRU information for the device */
+	CUC_CMD_SET_FAN_PWM = 26,               /* Set fan to fixed speed w/ given PWM duty cycle */
+	CUC_CMD_GET_FAN_RPM = 27,               /* Get the latest fan RPM value */
+	CUC_CMD_GET_MAC = 29,                   /* Get MAC addresses */
+	CUC_CMD_QSFP_READ = 31,                 /* Do a QSFP/AOC I2C Read */
+	CUC_CMD_QSFP_WRITE = 32,                /* Do a QSFP/AOC I2C Write */
+	CUC_CMD_QSFP_RESET = 33,                /* Do a QSFP/AOC module reset */
+	CUC_CMD_GET_INTR = 34,                  /* Get Interrupt Status and Enable bits for a NIC */
+	CUC_CMD_CLEAR_ISR = 35,                 /* Clear Interrupt Status bits for a NIC */
+	CUC_CMD_UPDATE_IER = 36,                /* Set/Clear Interrupt Enable bits for a NIC */
+	CUC_CMD_PLDM = 39,                      /* Generic entry point for PLDM transaction */
+	CUC_CMD_FIRMWARE_UPDATE_START = 46,     /* Update firmware */
+	CUC_CMD_FIRMWARE_VERSION = 51,          /* Get the version of a firmware component */
+	CUC_CMD_FIRMWARE_UPDATE_DOWNLOAD = 52,  /* Download the firmware */
+	CUC_CMD_FIRMWARE_UPDATE_STATUS = 53,    /* Get status of firmware update */
+	CUC_CMD_RESET = 54,                     /* Reset the uC */
+	CUC_CMD_SET_LED = 58,                   /* Control the blink pattern (state) of an LED */
+	CUC_CMD_GET_NIC_ID = 60                 /* Get the NIC ID associated with this interface */
+};
+
+enum {
+	CUC_TYPE_REQ,          /* Request packet */
+	CUC_TYPE_RSP_SUCCESS,  /* Success response packet */
+	CUC_TYPE_RSP_ERROR,    /* Error response packet */
+};
+
+struct cuc_error_rsp_data {
+	u8 error;  /* The POSIX errno that best describes the error */
+};
+
+struct cuc_set_fan_pwm_req_data {
+	/* PWM duty cycle value to set
+	 * 0- 100: Set to manual speed control
+	 * 255: Set to auto speed control
+	 */
+	u8 percent;
+} __packed;
+
+struct cuc_get_fan_rpm_rsp_data {
+	u32 rpm;     /* Measured fan speed in RPM */
+	u8 percent;  /* Current value of PWM duty cycle percent */
+	u8 is_auto;  /* Non-zero indicates auto speed control is active */
+} __packed;
+
+/* This is a special value that can be passed in the "nic" field of the
+ * cuc_mac_req_data structure to indicate to the uC that we want to get the
+ * MAC of the NIC that made the request
+ */
+#define CUC_MAC_THIS_NIC  0xFF
+
+struct cuc_mac_req_data {
+	u8 nic;  /* The NIC whose MAC addresses we want */
+} __packed;
+
+struct cuc_mac_rsp_data {
+	u8 nic;         /* The NIC */
+	u8 nic_mac[6];  /* The NIC MAC address */
+	u8 uc_mac[6];   /* The uC MAC address */
+} __packed;
+
+struct cuc_qsfp_read_req_data {
+	u8 nic;    /* Which NIC QSFP device to read */
+	u8 page;   /* Page select for reading */
+	u8 addr;   /* Start address for reading */
+	u8 count;  /* Number of bytes to read */
+} __packed;
+
+struct cuc_qsfp_read_rsp_data {
+	u8 data[0];
+} __packed;
+
+struct cuc_qsfp_write_req_data {
+	u8 nic;      /* Which NIC QSFP device to write */
+	u8 page;     /* Page select for writing */
+	u8 addr;     /* Start address for writing */
+	u8 count;    /* Number of bytes to write */
+	u8 data[0];  /* Data to be written */
+} __packed;
+
+struct cuc_qsfp_reset_req_data {
+	u8 nic;  /* Which NIC QSFP device to reset */
+} __packed;
+
+/* These are Interrupt Masks for interrupt sources that cause UC_ATTENTION[1] to be set in
+ * C_PI_ERR_FLG. Some of these interrupt sources are related to the ASIC power-off condition, in
+ * which case it won't be possible to generate the UC_ATTENTION[1] interrupt, but the interrupt
+ * status and mask values are still valid and maintained via the CasuC/CUC interface.
+ */
+/* Bits 0 through 7 are reserved for uC and board-related interrupts */
+#define ATT1_UC_RESET                   BIT(0)
+#define ATT1_SENSOR_ALERT               BIT(1)
+#define ATT1_FAN_FAIL                   BIT(2)
+/* Bits 8 through 15 are reserved for asic-related interrupts */
+#define ATT1_ASIC_PWR_UP_DONE           BIT(8)
+#define ATT1_ASIC_PWR_FAIL              BIT(9)
+#define ATT1_ASIC_EPO_TEMPERATURE       BIT(10)
+/* Bits 16 through 23 are reserved for qsfp-related interrupts */
+#define ATT1_QSFP_POWER_FAIL            BIT(16)
+#define ATT1_QSFP_INSERT                BIT(17)
+#define ATT1_QSFP_REMOVE                BIT(18)
+#define ATT1_QSFP_INT                   BIT(19)
+#define ATT1_QSFP_EPO_TEMPERATURE       BIT(20)
+/* Bits 24 through 31 are reserved for other future use */
+#define ATT1_ALL_INTERRUPTS             (0xFFFFFFFF)
+
+/* Most ATT1 interrupts are clearable by the host. Others are hardware-based and self-clearing */
+#define HOST_CLEARED_ATT1_INTERRUPTS  (ATT1_ALL_INTERRUPTS & ~(ATT1_QSFP_INT))
+
+struct cuc_get_intr_req_data {
+	u8 nic;  /* The NIC whose interrupts are being requested */
+} __packed;
+
+struct cuc_get_intr_rsp_data {
+	u32 isr;  /* Interrupt status register */
+	u32 ier;  /* Interrupt enable register */
+} __packed;
+
+struct cuc_clear_isr_req_data {
+	u8 nic;              /* The NIC whose interrupts are being cleared */
+	u32 isr_clear_bits;  /* Interrupt status bits to clear */
+} __packed;
+
+struct cuc_update_ier_req_data {
+	u8 nic;              /* The NIC who iterrupt enables are being updated */
+	u32 ier_set_bits;    /* Interrupt bits to enable */
+	u32 ier_clear_bits;  /* Interrupt bits to disable */
+} __packed;
+
+/* Two copies of the firmware are stored for each NIC, an active copy and a recovery copy */
+enum {
+	FW_SLOT_0,
+	FW_SLOT_ACTIVE = FW_SLOT_0,
+	FW_SLOT_1,
+	FW_SLOT_RECOVERY = FW_SLOT_1,
+	FW_SLOT_MAX
+};
+
+struct cuc_firmware_update_start_req {
+	u8 nic;    /* The NIC whose firmware is to be updated */
+	u32 size;  /* The size of the new firmware */
+	u8 slot;   /* The firmware slot to update */
+} __packed;
+
+/* The firmware (aka QSPI_BLOB) is composed of multiple, separately versioned components */
+enum casuc_fw_target {
+	FW_UC_APPLICATION,
+	FW_UC_BOOTLOADER,
+	FW_QSPI_BLOB,
+	FW_OPROM,
+	FW_CSR1,
+	FW_CSR2,
+	FW_SRDS,
+	FW_ISL68124_SAW,
+	FW_ISL68124_BRZ,
+	FW_IR38060,
+	FW_NUM_ENTRIES
+};
+
+struct cuc_get_firmware_version_req {
+	u8 fw_target;   /* The component whose version is being requested */
+	u8 nic;         /* The NIC whose firmware version is being requested */
+	u8 from_flash;  /* 0 = Get the running version, 1 = Get the stored version */
+	u8 slot;        /* The slot to get the version from, when getting stored version */
+} __packed;
+
+struct cuc_firmware_update_download_req {
+	u8 data[0];
+} __packed;
+
+enum {
+	FWU_STATUS_STARTED,             /* The firmware update has been started */
+	FWU_STATUS_DOWNLOADING,         /* The firmware is being downloaded */
+	FWU_STATUS_VERIFYING_SIGNATURE, /* The firmware signature is being validated */
+	FWU_STATUS_VALIDATING_IMAGE,    /* The firmware is being validated */
+	FWU_STATUS_FLASHING,            /* The firmware is being flashed */
+	FWU_STATUS_VERIFYING_FLASH,     /* The firmware flash is being verified */
+	FWU_STATUS_IDLE = 0x80,
+	FWU_STATUS_SUCCESS,             /* The firmware update succeeded */
+	FWU_STATUS_FAILED,              /* The firmware update failed */
+	FWU_STATUS_FAILED_DOWNLOAD,     /* The firmware download failed */
+	FWU_STATUS_FAILED_BAD_SIGN,     /* The firmware signature was incorrect */
+	FWU_STATUS_FAILED_VALIDATION,   /* Failure validating image before flashing */
+	FWU_STATUS_FAILED_FLASH,        /* Failure during flash */
+	FWU_STATUS_FAILED_VERIFICATION, /* Failure verifying image in flash */
+	FWU_STATUS_FAILED_INVALID_SLOT, /* The firmware slot is not valid */
+};
+
+struct cuc_firmware_update_status_rsp {
+	u8 status;  /* Status of the firmware update state-machine */
+} __packed;
+
+enum casuc_leds {
+	LED_LINK_STATUS = 0,
+};
+
+/* GRN represents the nominal color and YEL represents the alternate color. Slow blinking
+ * frequency is 1Hz, fast is 2Hz.
+ */
+enum casuc_led_states {
+	LED_OFF = 0,
+	LED_ON_GRN = 1,
+	LED_SLOW_GRN = 2,
+	LED_FAST_GRN = 3,
+	LED_ON_YEL = 4,
+	LED_SLOW_YEL = 5,
+	LED_FAST_YEL = 6,
+	LED_SLOW_GRN_YEL = 7,
+	LED_FAST_GRN_YEL = 8,
+};
+
+struct cuc_set_led_req {
+	u8 nic;    /* The NIC whose LED state is to be set */
+	u8 led;    /* The LED whose state is to be set */
+	u8 state;  /* The state to set */
+} __packed;
+
+struct cuc_get_nic_id_rsp {
+	u8 nic;  /* The requested NIC ID */
+} __packed;
+
+#endif /* CUC_CXI_H */
---
+++ lib/craypldm/pldm_cxi.h
@@ -0,0 +1,316 @@
+/* SPDX-License-Identifier: MIT */
+/* Copyright 2021 Hewlett Packard Enterprise Development LP */
+
+/* This file contains Platform Level Data Model (PLDM) definitions necessary to read numeric
+ * sensors and parse FRU record data from the Cassini uC PLDM subsystem.
+ *
+ * More information can be found in the following DMTF documents (https://www.dmtf.org):
+ * DSP0240 v1.0.0: PLDM Base Specification
+ * DSP0245 v1.3.0: PLDM IDs and Codes Specification
+ * DSP0248 v1.2.0: PLDM for Platform Monitoring and Control Specification
+ * DSP0257 v1.0.0: PLDM for FRU Data Specification
+ */
+
+#ifndef PLDM_CXI_H
+#define PLDM_CXI_H
+
+/* Generic PLDM Message Fields (DSP0240 Figure 1) */
+struct pldm_hdr {
+	u8 instance_id:5;
+	u8 rsvd:1;
+	u8 d:1;
+	u8 rq:1;
+
+	u8 pldm_type:6;
+	u8 hdr_ver:2;
+
+	u8 pldm_command_code;
+	u8 payload[0];
+} __packed;
+
+/* PLDM Type Codes (DSP0245 Table 1) */
+enum pldm_type_code {
+	PLDM_TYPE_MESSAGING_CONTROL_AND_DISCOVERY = 0,
+	PLDM_TYPE_SMBIOS,
+	PLDM_TYPE_PLATFORM_MONITORING_AND_CONTROL,
+	PLDM_TYPE_BIOS_CONTROL_AND_CONFIGURATION,
+	PLDM_TYPE_FRU_DATA,
+	PLDM_TYPE_FIRMWARE_UPDATE,
+	PLDM_TYPE_REDFISH_DEVICE,
+	PLDM_TYPE_OEM = 0x3F,
+	PLDM_TYPE_COUNT
+};
+
+/* Generic PLDM Completion Codes (DSP0240 Table 4) */
+enum pldm_completion_code {
+	PLDM_SUCCESS = 0,
+	PLDM_ERROR = 1,
+	PLDM_ERROR_INVALID_DATA = 2,
+	PLDM_ERROR_INVALID_LENGTH = 3,
+	PLDM_ERROR_NOT_READY = 4,
+	PLDM_ERROR_UNSUPPORTED_CMD = 5,
+	PLDM_ERROR_INVALID_PLDM_TYPE = 6,
+	PLDM_ERROR_COMMAND_SPECIFIC_START = 0x80,
+	PLDM_ERROR_COMMAND_SPECIFIC_END = 0xFF
+};
+
+/* PLDM Command Numbers (DSP0248 Table 107) */
+enum pldm_platform_cmd {
+	PLDM_CMD_GET_SENSOR_READING = 0x11,
+	PLDM_CMD_GET_PDR = 0x51
+};
+
+/* GetSensorReading presentReading variable types (DSP0248 Table 30) */
+union sensor_value {
+	s8 value_SINT8;
+	u8 value_UINT8;
+	s16 value_SINT16;
+	u16 value_UINT16;
+	s32 value_SINT32;
+	u32 value_UINT32;
+};
+
+/* GetSensorReading Command Request Format (DSP0248 Table 30) */
+struct get_sensor_reading_req {
+	struct pldm_hdr hdr;
+	u16 sensor_id;
+	u8 rearm_event_status;
+} __packed;
+
+/* GetSensorReading Command Response Format (DSP0248 Table 30) */
+struct get_sensor_reading_rsp {
+	struct pldm_hdr hdr;
+	u8 completion_code;
+	u8 sensor_data_size;
+	u8 sensor_operational_state;
+	u8 sensor_event_message_enable;
+	u8 present_state;
+	u8 previous_state;
+	u8 event_state;
+	union sensor_value present_reading;
+} __packed;
+
+/* GetPDR Command Request Format (DSP0248 Table 68) */
+struct get_pdr_req {
+	struct pldm_hdr hdr;
+	u32 record_handle;
+	u32 data_transfer_handle;
+	u8 transfer_operation_flag;
+	u16 request_count;
+	u16 record_change_number;
+} __packed;
+
+/* GetPDR Command Response Format (DSP0248 Table 68) */
+struct get_pdr_rsp {
+	struct pldm_hdr hdr;
+	u8 completion_code;
+	u32 next_record_handle;
+	u32 next_data_transfer_handle;
+	u8 transfer_flag;
+	u16 response_count;
+	u8 record_data[0];
+} __packed;
+
+/* GetPDR transferOperationFlag values (DSP0248 Table 68) */
+enum pldm_transfer_op {
+	PLDM_XFER_OP_GET_NEXT_PART = 0,
+	PLDM_XFER_OP_GET_FIRST_PART = 1
+};
+
+/* Common PDR Header Format (DSP0248 Table 75) */
+struct pdr_hdr {
+	u32 record_handle;
+	u8 pdr_header_version;
+	u8 pdr_type;
+	u16 record_change_number;
+	u16 data_length;
+	u8 data[0];
+} __packed;
+
+/* PDR Type Values (DSP0248 Table 76) */
+enum pdr_type {
+	PLDM_PDR_TERMINUS_LOCATOR = 1,
+	PLDM_PDR_NUMERIC_SENSOR = 2,
+	PLDM_PDR_STATE_SENSOR = 4,
+	PLDM_PDR_SENSOR_AUXILIARY_NAMES = 6,
+	PLDM_PDR_STATE_EFFECTER = 11,
+	PLDM_PDR_EFFECTER_AUXILIARY_NAMES = 13,
+	PLDM_PDR_ENTITY_ASSOCIATION = 15,
+	PLDM_PDR_ENTITY_AUXILIARY_NAMES = 16,
+	PLDM_PDR_FRU_RECORD_SET = 20,
+	PLDM_PDR_OEM = 127
+};
+
+/* Numeric Sensor PDR sensorDataSize values (DSP0248 Table 78) */
+enum pldm_data_size {
+	PLDM_DATA_SIZE_UINT8,
+	PLDM_DATA_SIZE_SINT8,
+	PLDM_DATA_SIZE_UINT16,
+	PLDM_DATA_SIZE_SINT16,
+	PLDM_DATA_SIZE_UINT32,
+	PLDM_DATA_SIZE_SINT32,
+};
+
+/* The Numeric Sensor PDR has variably sized fields depending on the size of the sensor reading.
+ * To keep the structure packing as simple as possible, unions are used for everything downstream
+ * of the first variably sized field. These sensor size dependent structs are only defined for a
+ * given size (not signedness). They include all Numeric Sensor PDR fields from 'hysteresis' to
+ * 'fatal_low' (DSP0248 Table 78)
+ */
+struct numeric_sensor_ssd8 {
+	u8 hysteresis;
+	u8 supported_thresholds;
+	u8 threshold_and_hysteresis_volatility;
+	float state_transition_interval;
+	float update_interval;
+	u8 max_readable;
+	u8 min_readable;
+	u8 range_field_format;
+	u8 range_field_support;
+	u8 nominal_value;
+	u8 normal_max;
+	u8 normal_min;
+	u8 warning_high;
+	u8 warning_low;
+	u8 critical_high;
+	u8 critical_low;
+	u8 fatal_high;
+	u8 fatal_low;
+} __packed;
+
+struct numeric_sensor_ssd16 {
+	u16 hysteresis;
+	u8 supported_thresholds;
+	u8 threshold_and_hysteresis_volatility;
+	float state_transition_interval;
+	float update_interval;
+	u16 max_readable;
+	u16 min_readable;
+	u8 range_field_format;
+	u8 range_field_support;
+	u16 nominal_value;
+	u16 normal_max;
+	u16 normal_min;
+	u16 warning_high;
+	u16 warning_low;
+	u16 critical_high;
+	u16 critical_low;
+	u16 fatal_high;
+	u16 fatal_low;
+} __packed;
+
+struct numeric_sensor_ssd32 {
+	u32 hysteresis;
+	u8 supported_thresholds;
+	u8 threshold_and_hysteresis_volatility;
+	float state_transition_interval;
+	float update_interval;
+	u32 max_readable;
+	u32 min_readable;
+	u8 range_field_format;
+	u8 range_field_support;
+	u32 nominal_value;
+	u32 normal_max;
+	u32 normal_min;
+	u32 warning_high;
+	u32 warning_low;
+	u32 critical_high;
+	u32 critical_low;
+	u32 fatal_high;
+	u32 fatal_low;
+} __packed;
+
+/* Numeric Sensor PDR Format (DSP0248 Table 78) */
+struct numeric_sensor_pdr {
+	struct pdr_hdr hdr;
+	u16 pldm_terminus_handle;
+	u16 sensor_id;
+	union {
+		struct {
+			u16 entity_type_entity_id :15;
+			u16 entity_type_logical   :1;
+		};
+		u16 entity_type;
+	};
+	u16 entity_instance_number;
+	u16 container_id;
+	u8 sensor_init;
+	u8 sensor_auxiliary_names_pdr;
+	u8 base_unit;
+	s8 unit_modifier;
+	u8 rate_unit;
+	u8 base_oem_unit_handle;
+	u8 aux_unit;
+	s8 aux_unit_modifier;
+	u8 aux_rate_unit;
+	u8 rel;
+	u8 aux_oem_unit_handle;
+	u8 is_linear;
+	u8 sensor_data_size;
+	float resolution;
+	float offset;
+	u16 accuracy;
+	u8 plus_tolerance;
+	u8 minus_tolerance;
+	union {
+		struct numeric_sensor_ssd8 ssd8;
+		struct numeric_sensor_ssd16 ssd16;
+		struct numeric_sensor_ssd32 ssd32;
+	} ssd;
+} __packed;
+
+#define AUX_NAME_MAX  33
+
+/* Sensor Auxiliary Names PDR Format (DSP0248 Table 83) */
+struct aux_name_pdr {
+	struct pdr_hdr hdr;
+	u16 pldm_terminus_handle;
+	u16 sensor_id;
+	u8 sensor_count;
+	u8 name_string_count;           /* Currently hard-coded to a single name */
+	u8 name_language_tag[3];        /* Currently hard-coded to "en" only */
+	u16 sensor_name[AUX_NAME_MAX];
+} __packed;
+
+/* PLDM FRU Field TLV (DSP0257 Table 2) */
+struct pldm_fru_field {
+	u8 field_type;
+	u8 length;
+	u8 value[0];
+} __packed;
+
+/* PLDM FRU Record Data Format (DSP0257 Table 2) */
+struct pldm_fru_record {
+	u16 record_set_id;
+	u8 record_type;
+	u8 num_fields;
+	u8 field_encoding;
+	u8 field_data[0];
+} __packed;
+
+/* FRU Record Types (DSP0257 Table 4) */
+enum pldm_fru_record_type {
+	PLDM_FRU_RECORD_GENERAL = 1,
+	PLDM_FRU_RECORD_OEM = 254
+};
+
+/* General FRU Record Field Types (DSP0257 Table 5) */
+enum pldm_fru_field_type {
+	PLDM_FRU_FIELD_CHASSIS_TYPE = 1,
+	PLDM_FRU_FIELD_MODEL = 2,
+	PLDM_FRU_FIELD_PART_NUMBER = 3,
+	PLDM_FRU_FIELD_SERIAL_NUMBER = 4,
+	PLDM_FRU_FIELD_MANUFACTURER = 5,
+	PLDM_FRU_FIELD_MANUFACTURE_DATE = 6,
+	PLDM_FRU_FIELD_VENDOR = 7,
+	PLDM_FRU_FIELD_NAME = 8,
+	PLDM_FRU_FIELD_SKU = 9,
+	PLDM_FRU_FIELD_VERSION = 10,
+	PLDM_FRU_FIELD_ASSET_TAG = 11,
+	PLDM_FRU_FIELD_DESCRIPTION = 12,
+	PLDM_FRU_FIELD_ENGINEERING_CHANGE_LEVEL = 13,
+	PLDM_FRU_FIELD_OTHER = 14,
+	PLDM_FRU_FIELD_VENDOR_IANA = 15
+};
+
+#endif /* PLDM_CXI_H */
